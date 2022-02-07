5fe5157050b102e1cb97bcfdb1f4ca947197969e
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2022-02-07 09:15:07 +0530

    Primera

---
+++ chatbotserver.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import {Server} from 'socket.io'
+
+const app=express()
+
+app.use(cors)
+
+const port = process.env.PORT || 3001
+const server = http.createServer(app)
+const io = new Server(server)
+
+io.on("connection",(socket)=>{
+    console.log(`User ID:${socket.id} has connected... `)
+
+    socket.on("joinRoom",(data)=>{
+        socket.join(data.room)
+        console.log(`User: ${data.username} has joined room: ${data.room}`)
+    })
+    socket.on("sendMessage",(data)=>{ 
+        socket.to(data.room).emit("recieveMessage",data)
+        console.log(data.room)
+    })
+    socket.on("disconnect",()=>{
+        console.log(`User ID:${socket.id} has disconnected... `)
+    })
+})
+
+server.listen(port,()=>{
+    console.log('Server is Online...')
+})
+
